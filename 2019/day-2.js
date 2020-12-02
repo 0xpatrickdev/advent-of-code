a6cd69d0e36d18e29a12e4e6facc8bd9e230a770
Patrick Cooney <patrick.ocooney@aexp.com>
2020-12-02 18:37:58 -0500

    (2019-day-2): 1212 program alarm

---
+++ 2019/day-2.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+function getData() {
+  return fs.readFileSync('2019/day-2.txt', {encoding: 'utf-8'}).split(',').map((x) => parseInt(x));
+}
+
+function getPartOneAnswer(key1Value = 12, key2Value = 2) {
+  const data = getData();
+
+  data[1] = key1Value;
+  data[2] = key2Value;
+
+  for (let i = 0; i < data.length; i++) {
+    if ((i + 1) % 4 === 0) {
+      const opcode = data[i - 3];
+      const targetIndex = data[i];
+      switch (opcode) {
+        case 1:
+          // 1: add
+          data[targetIndex] = data[data[i - 2]] + data[data[i - 1]];
+          break;
+        case 2:
+          // 2: multiply
+          data[targetIndex] = data[data[i - 2]] * data[data[i - 1]];
+          break;
+        case 99:
+          // 99: end program now
+          return data[0];
+        default:
+          throw new Error('inavlid opcode');
+      }
+    }
+  }
+  return data[0];
+}
+
+function getPartTwoAnswer(targetOutput = 19690720) {
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      if (getPartOneAnswer(noun, verb) === targetOutput) {
+        return 100 * noun + verb;
+      }
+    }
+  }
+}
+
+console.log('Part One', getPartOneAnswer());
+console.log('Part Two', getPartTwoAnswer());
