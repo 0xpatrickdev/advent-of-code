6f16396a3f431c0226265173d307a7101681af36
Patrick Cooney <patrick.ocooney@aexp.com>
2020-12-03 10:11:18 -0500

    (2020-day-2): cleanup

--- 2020/day-2.js
+++ 2020/day-2.js
@@ -8,8 +8,8 @@ function formatData(arr) {
   return arr.map((x) => {
     const [minMax, letter, password] = x.split(' ');
     return {
-      min: minMax.split('-')[0],
-      max: minMax.split('-')[1],
+      min: Number(minMax.split('-')[0]),
+      max: Number(minMax.split('-')[1]),
       letter: letter.slice(0, 1),
       password,
     };
@@ -22,6 +22,7 @@ function getPartOneAnswer() {
 
   for (let i = 0; i < data.length; i++) {
     const { min, max, letter, password } = data[i];
+    // ensure count of letter in password is min <= count <= max
     const count = (password.match(new RegExp(letter, 'g')) || []).length;
     if (count >= min && count <= max) {
       validPasswordCount++;
@@ -37,9 +38,10 @@ function getPartTwoAnswer() {
 
   for (let i = 0; i < data.length; i++) {
     const { min: first, max: second, letter, password } = data[i];
-    const firstIsMatch = password.split('')[parseInt(first) - 1] === letter;
-    const secondIsMatch = password.split('')[parseInt(second) - 1] === letter;
+    const firstIsMatch = password[first - 1] === letter;
+    const secondIsMatch = password[second - 1] === letter;
 
+    // cast bool's to numbers and only return if exactly one is true
     if (Number(firstIsMatch) + Number(secondIsMatch) === 1) {
       validPasswordCount++;
     }
