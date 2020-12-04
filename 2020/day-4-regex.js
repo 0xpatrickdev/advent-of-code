64bddca4a35be12de9e321fefd9fad5bd8856f3c
Patrick Cooney <patrick.ocooney@aexp.com>
2020-12-04 16:09:53 -0500

    (2020-day-4): passports with regex only

---
+++ 2020/day-4-regex.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+function readFile() {
+  return fs.readFileSync('2020/day-4.txt', 'utf-8');
+}
+
+function getPassports() {
+  return JSON.parse(
+    `[{"${readFile()
+      .replace(/\n\n/g, '"},{"')
+      .replace(/\n/g, ' ')
+      .replace(/ /g, '","')
+      .replace(/:/g, '":"')}"}]`
+  );
+}
+
+const validatorMap = {
+  // 1920 - 2002
+  byr: /^(19[2-9][0-9]|200[0-2])$/,
+  // 2010 - 2020
+  iyr: /^20(1[0-9]|20)$/,
+  // 2020 - 230
+  eyr: /^20(2[0-9]|30)$/,
+  // 150-193cm or 59-76in
+  hgt: /^(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$/,
+  // valid color hex
+  hcl: /^#[a-f0-9]{6}$/,
+  // one of 7 eye colors
+  ecl: /(amb|blu|brn|gry|grn|hzl|oth)/,
+  // exactly 9 digits
+  pid: /^[0-9]{9}$/,
+};
+
+const validatorKeys = Object.keys(validatorMap);
+
+const hasValidField = (key, passport) => !!passport[key];
+
+const matchesRegex = (key, val) => !!val.match(validatorMap[key]);
+
+function getValidPassportCount(requiredOnly = false) {
+  return getPassports().filter((p) =>
+    validatorKeys.every((key) =>
+      requiredOnly
+        ? hasValidField(key, p)
+        : hasValidField(key, p) && matchesRegex(key, p[key])
+    )
+  ).length;
+}
+
+function getPartOneAnswer() {
+  return getValidPassportCount(true);
+}
+
+function getPartTwoAnswer() {
+  return getValidPassportCount();
+}
+
+console.log('Part One', getPartOneAnswer());
+console.log('Part Two', getPartTwoAnswer());
