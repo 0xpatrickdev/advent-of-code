4e192bb9b89713d8cba9d54241c8c6d0f90fa0fb
Patrick Cooney <pcooney10@gmail.com>
2020-12-08 13:50:24 -0500

    (2020 day 8): handheld halting refactor
    - (p2) edit instructions array in place instead of creating a new one each time

--- 2020/day-8.js
+++ 2020/day-8.js
@@ -1,19 +1,13 @@
 const fs = require('fs');
 
 function getData() {
-  return fs
-    .readFileSync('2020/day-8.txt', 'utf-8')
-    .split('\n')
-    .map((x) => ({
-      operation: x.split(' ')[0],
-      argument: x.split(' ')[1],
-      executed: false,
-    }));
+  return fs.readFileSync('2020/day-8.txt', 'utf-8').split('\n');
 }
 
 function getPartOneAnswer(instructions = getData()) {
   let accumulator = 0;
   let pointer = 0;
+  const executed = new Set();
 
   while (true) {
     // return if we've hit the last line
@@ -21,10 +15,11 @@ function getPartOneAnswer(instructions = getData()) {
       return { accumulator, endsLastLine: true };
     }
 
-    const { operation, argument, executed } = instructions[pointer];
-    // return if we've already executed a command before
-    if (executed) return accumulator;
-    instructions[pointer]['executed'] = true;
+    // return if we've already executed a command
+    if (executed.has(pointer)) return accumulator;
+    executed.add(pointer);
+
+    const [operation, argument] = instructions[pointer].split(' ');
 
     switch (operation) {
       case 'acc':
@@ -43,18 +38,28 @@ function getPartOneAnswer(instructions = getData()) {
   }
 }
 
+const flipOp = (op) => (op === 'nop' ? 'jmp' : 'nop');
+
 function getPartTwoAnswer() {
   const instructions = getData();
 
+  let prevPointer;
   // edit one instruction to find the program that ends on the last line
   for (i in instructions) {
-    const { operation } = instructions[i];
-    if (operation !== 'acc') {
-      const copy = getData();
-      copy[i].operation = operation === 'nop' ? 'jmp' : 'nop';
-      const { accumulator, endsLastLine } = getPartOneAnswer(copy);
-      if (endsLastLine) return accumulator;
+    const [operation, argument] = instructions[i].split(' ');
+    // only update if 'nop' or 'jmp'
+    if (operation === 'acc') continue;
+    // update operation(s) in place
+    if (prevPointer) {
+      const [prevOp, prevArg] = instructions[prevPointer].split(' ');
+      instructions[prevPointer] = flipOp(prevOp) + ' ' + prevArg;
     }
+    instructions[i] = flipOp(operation) + ' ' + argument;
+    prevPointer = i;
+
+    // check for when program ends on last line
+    const { accumulator, endsLastLine } = getPartOneAnswer(instructions);
+    if (endsLastLine) return accumulator;
   }
 }
 
